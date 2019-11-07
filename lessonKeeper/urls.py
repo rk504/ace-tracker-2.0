7d1c37b1f6f45e931f34a124e564516c0eae9061
spiano <lafleurpiano@gmail.com>
2019-11-06 18:21:32 -0600

    full CRUD with navigation

--- lessonKeeper/urls.py
+++ lessonKeeper/urls.py
@@ -5,8 +5,11 @@ urlpatterns = [
     path('', views.student_list, name = 'student_list'),
     path('students/<int:pk>', views.student_detail, name = 'student_detail'),
     path('students/new', views.student_create, name = 'student_create'),
+    path('students/<int:pk>/edit', views.student_edit, name = 'student_edit'),
+    path('students/<int:pk>/delete', views.student_delete, name = 'student_delete'),
     path('contacts/', views.contact_list, name = 'contact_list'),
     path('contacts/<int:pk>', views.contact_detail, name = 'contact_detail'),
     path('contacts/new', views.contact_create, name = 'contact_create'),
-
+    path('contacts/<int:pk>/edit', views.contact_edit, name = 'contact_edit'),
+    path('contacts/<int:pk>/delete', views.contact_delete, name = 'contact_delete'),
 ]
--- lessonKeeper/views.py
+++ lessonKeeper/views.py
@@ -21,6 +21,21 @@ def student_create(request):
         form = StudentForm()
     return render(request, 'student_form.html', {'form': form})
 
+def student_edit(request, pk):
+    student = Student.objects.get(pk=pk)
+    if request.method == "POST":
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            student = form.save()
+            return redirect('student_detail', pk=student.pk)
+    else:
+        form = StudentForm(instance=student)
+    return render(request, 'student_form.html', {'form': form})
+
+def student_delete(request, pk):
+    Student.objects.get(id=pk).delete()
+    return redirect('student_list')
+
 def contact_list(request):
     contacts = Contact.objects.all()
     return render(request, 'contact_list.html', {'contacts': contacts})
@@ -38,3 +53,18 @@ def contact_create(request):
     else:
         form = ContactForm()
     return render(request, 'contact_form.html', {'form': form})
+
+def contact_edit(request, pk):
+    contact = Contact.objects.get(pk=pk)
+    if request.method == "POST":
+        form = ContactForm(request.POST, instance=contact)
+        if form.is_valid():
+            contact = form.save()
+            return redirect('contact_detail', pk=contact.pk)
+    else:
+        form = ContactForm(instance=contact)
+    return render(request, 'contact_form.html', {'form': form})
+
+def contact_delete(request, pk):
+    Contact.objects.get(id=pk).delete()
+    return redirect('contact_list')
