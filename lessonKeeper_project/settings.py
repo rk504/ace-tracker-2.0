b21fa0cc045365d1445c0bf55714f907716a0727
spiano <lafleurpiano@gmail.com>
2019-12-08 07:29:31 -0600

    added cors to see if that fixes deployment issues

--- lessonKeeper_project/settings.py
+++ lessonKeeper_project/settings.py
@@ -39,12 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lessonKeeper',
+    'corsheaders',
     'crispy_forms',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
