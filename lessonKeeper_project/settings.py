7430460208679968082988b75848a8445bad3734
spiano <lafleurpiano@gmail.com>
2020-01-08 11:07:05 -0600

    updated key for production

--- lessonKeeper_project/settings.py
+++ lessonKeeper_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gp^*ynl-!%b8^)oia!s)k#4)(y&0*8=wj+j@zv=kk)-s@)#3ft'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
